5c0de09d9cfcacbf8401902f6bec70d968956046
Denis <deninichi@gmail.com>
2018-08-13 00:38:56 +0400

    Setup user points after each payment

---
+++ classes/class-user-woocommerce-points.php
@@ -0,0 +1,86 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
+/**
+ * User Points plugin points calculation Class
+ *
+ *
+ * @package WordPress
+ * @subpackage User_Points
+ * @category Plugin
+ * @author Denis Nichik
+ * @since 1.0.0
+ */
+class Woo_Points {
+
+	/**
+	 * Constructor function.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		
+		add_action( 'woocommerce_order_status_completed', array( $this, 'mycred_pro_reward_order_points' ), 20 );
+
+		add_action( 'after_setup_theme', array( $this, 'mycred_pro_adjust_woo_rewards'), 110 );
+
+	} // End __construct()
+
+	/**
+	 * Set points value after each payment
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function mycred_pro_reward_order_points( $order_id ){
+
+		if ( ! function_exists( 'mycred' ) ) return;
+
+		// Get Order
+		$order   = wc_get_order( $order_id );
+
+		$items_number = 0;
+		foreach ( $order->get_items() as $item_id => $item ) {
+			$quantity = $item->get_quantity();
+			$items_number += $quantity;
+		}
+
+		// Load myCRED
+		$mycred  = mycred();
+
+		// Do not payout if order was paid using points
+		if ( $order->get_payment_method() == 'mycred' ) return;
+
+		if ( ! $mycred ) return;
+
+		// Make sure user only gets points once per order
+		if ( $mycred->has_entry( 'reward', $order_id, $order->get_user_id() ) ) return;
+
+		// Reward example 10 * order items in points.
+		$reward  = 10 * $items_number;
+
+		// Add reward
+		$mycred->add_creds(
+			'reward',
+			$order->get_user_id(),
+			$reward,
+			'Reward for store purchase',
+			$order_id,
+			array( 'ref_type' => 'post' )
+		);
+	}
+
+
+	/**
+	 * Set points value after each payment
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function mycred_pro_adjust_woo_rewards() {
+
+		remove_action( 'woocommerce_payment_complete',    'mycred_woo_payout_rewards' );
+		add_action( 'woocommerce_order_status_completed', 'mycred_woo_payout_rewards' );
+
+	}
+
+}
+
+new Woo_Points();
---
+++ user-points.php
@@ -0,0 +1,126 @@
+<?php 
+
+/**
+ * Plugin Name: User Points
+ * Plugin URI: http://deninichi.com/user-points/
+ * Description: MyCred user points add-on
+ * Version: 1.0.0
+ * Author: Denis Nichik
+ * Author URI: http://deninichi.com/
+ * Requires at least: 4.0.0
+ * Tested up to: 4.0.0
+ *
+ * Text Domain: user-points
+ * Domain Path: /languages/
+ *
+ * @package User_Points
+ * @category Core
+ * @author Denis Nichik
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+/**
+ * Returns the main instance of User_Points to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object User_Points
+ */
+function User_Points() {
+	return User_Points::instance();
+} // End User_Points()
+add_action( 'plugins_loaded', 'User_Points' );
+
+
+/**
+ * Main User_Points Class
+ *
+ * @class User_Points
+ * @version	1.0.0
+ * @since 1.0.0
+ * @package	User_Points
+ * @author Denis Nichik
+ */
+final class User_Points {
+
+	/**
+	 * User_Points The single instance of User_Points.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+	
+	/**
+	 * The token.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $token;
+
+	/**
+	 * The version number.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $version;
+
+	/**
+	 * The plugin directory URL.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $plugin_url;
+
+	/**
+	 * The plugin directory path.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $plugin_path;
+
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function __construct () {
+		$this->token 			= 'user-point';
+		$this->plugin_url 		= plugin_dir_url( __FILE__ );
+		$this->plugin_path 		= plugin_dir_path( __FILE__ );
+		$this->version 			= '1.0.0';
+
+	// Woocommerce points
+	require_once( 'classes/class-user-woocommerce-points.php' );
+
+	} // End __construct()
+
+
+	/**
+	 * Main User_Points Instance
+	 *
+	 * Ensures only one instance of User_Points is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see User_Points()
+	 * @return Main User_Points instance
+	 */
+	public static function instance () {
+		
+		if ( is_null( self::$_instance ) ){
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	} // End instance()
+
+
+} // End Class
+
+$userPoints = new User_Points();
